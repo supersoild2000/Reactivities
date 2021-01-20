cb1974e019b4c9a9491b760c473eb4c4ba87e42c
Hryhorii Pazych <supersoild2000@gmail.com>
2021-01-20 22:55:32 +0200

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -20,9 +20,20 @@ class ActivityStore {
   @observable target = "";
 
   @computed get activitiesByDate() {
-    return Array.from(this.activityRegistry.values()).sort(
+    return this.groupActivitiesByDate(
+      Array.from(this.activityRegistry.values())
+    );
+  }
+
+  groupActivitiesByDate(acitivities: IActivity[]) {
+    const sortedActivities = acitivities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
+    return Object.entries(sortedActivities.reduce((activities,activity)=>{
+      const date = activity.date.split('T')[0];
+      activities[date] = activities[date] ? [...activities[date], activity] : [activity]
+      return activities;
+    },{} as {[key: string]: IActivity[]}));
   }
 
   @action loadActivities = async () => {
@@ -109,28 +120,28 @@ class ActivityStore {
       this.activity = activity;
     } else {
       this.loadingInitial = true;
-      try{
+      try {
         activity = await agent.Activities.details(id);
-        runInAction(()=>{
+        runInAction(() => {
           this.activity = activity;
           this.loadingInitial = false;
-        })
-      } catch (error){
-        runInAction(()=>{
+        });
+      } catch (error) {
+        runInAction(() => {
           this.loadingInitial = false;
-        })
+        });
         console.log(error);
       }
     }
   };
-  
+
   @action clearActivity = () => {
     this.activity = null;
-  }
+  };
 
   getActivity = (id: string) => {
     return this.activityRegistry.get(id);
-  }
+  };
 
   constructor() {
     makeAutoObservable(this);
